1d1390d83837998fe6e716e25de4be85898829de
YangShuailing <YangShuailing@outlook.com>
2023-05-09 10:44:25 +0800

    ----version 5.4.2---- 简单测试JdbcTemplate

--- src/test/java/com/njfu/edu/SpringTest.java
+++ src/test/java/com/njfu/edu/SpringTest.java
@@ -3,13 +3,17 @@ package com.njfu.edu;
 import com.njfu.edu.service.CheckPersonService;
 import com.njfu.edu.service.impl.CheckPersonServiceImpl;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 public class SpringTest {
     @Test
-    public void test01() throws SQLException {
+    public void aop() throws SQLException {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         CheckPersonService checkPersonService = classPathXmlApplicationContext.getBean("checkPersonService", CheckPersonService.class);
 
@@ -17,7 +21,13 @@ public class SpringTest {
     }
 
     @Test
-    public void AopTest(){
-
+    public void jdbcTemplate(){
+        //启动IoC容器
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取IoC容器中JdbcTemplate实例
+        JdbcTemplate jdbcTemplate=(JdbcTemplate) ctx.getBean("jdbcTemplate");
+        String sql="select * from lessontraining.student";
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        System.out.println(maps);
     }
 }
